2ca30bc0cec477db5efd9321ec56915884901a7b
CorneliusNgure <kingcornelius07@gmail.com>
2023-03-06 16:30:54 -0500

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
 /**
  * main - A program printing a line to a stsndard error.
  * Return 1 (Successful)
- */
+*/
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+return (1)
 }
