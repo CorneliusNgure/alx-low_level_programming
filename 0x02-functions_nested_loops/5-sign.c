03bc26cb9f2003d2701b085478ed66457cfa7eab
CorneliusNgure <kingcornelius07@gmail.com>
2023-03-07 05:18:36 -0500

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar(43);
+_ putchar(43);
 return (1);
 }
 else if (n < 0)
@@ -21,7 +21,7 @@ return (-1);
 }
 else
 {
-putchar(48);
+_putchar(48);
 return (0);
 }
 }
