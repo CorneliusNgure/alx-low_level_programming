732ac5b6001ace43e569dcc9cb2ccb31e684a908
CorneliusNgure <kingcornelius07@gmail.com>
2023-06-19 03:30:48 -0400

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int alphabet = 'a';
+
+	while (alphabet < 'z')
+	{
+		if (alphabet == 'e')
+		{
+			alphabet++;
+			continue;
+		}
+		else if (alphabet == 'q')
+		{
+			alphabet++;
+			continue;
+		}
+			putchar(alphabet);
+			alphabet++;
+			putchar('\n');
+	}
+	return (0);
+}
