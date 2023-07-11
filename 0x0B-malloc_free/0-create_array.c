e5699775951fb17d0e739a9e36222aac81942ee9
CorneliusNgure <kingcornelius07@gmail.com>
2023-07-11 16:52:09 -0400

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,8 +17,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ptr = malloc(sizeof(char) * size);
-	if (size == 0 || ptr == NULL)
-		exit(0);
+	if (ptr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
