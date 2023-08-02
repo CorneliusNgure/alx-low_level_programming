2023d44079d596091033da9f666b95ca5c577491
CorneliusNgure <kingcornelius07@gmail.com>
2023-08-02 06:46:31 -0400

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,23 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
-struct dog {
+/**
+ * struct dog - name of the structure
+ *
+ * @name: name of the first memeber of the structure.
+ * @age: name of the second element of the structure.
+ * @owner: name of the third element of the strucure.
+ *
+ * Description: Something to do with a dog.
+ *
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
