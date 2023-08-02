30f8aa3587aeb2cc94ef353f9e1256606fd7ea77
CorneliusNgure <kingcornelius07@gmail.com>
2023-08-02 08:04:44 -0400

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,10 +19,13 @@ struct dog
 	char *owner;
 };
 
-/**dog_t - a new struct for struct dog.
+/**
+ * dog_t - a new struct for struct dog.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
