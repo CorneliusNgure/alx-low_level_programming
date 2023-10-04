3d784ce2b9e4ea3419f0491afa9d907155d9b429
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-04 11:05:04 -0400

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program.
+ * @argc: the no. of arguments passed.
+ * @argv: pointer to the strings of arguments.
+ * Return: 0 (Success).
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if ( argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+
+	return (0);
+}
