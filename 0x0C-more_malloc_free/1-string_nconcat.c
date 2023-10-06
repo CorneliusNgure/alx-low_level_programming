8d7f3377af6e7dedd316bb2a5c9c8c6c4f1db9bf
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-06 12:12:54 -0400

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,54 @@
-#include "main.h"
-#include <string.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * string_nconcat - concatenate two strings.
- * @s1: first string.
- * @s2: second string.
- * @n: size of the malloc allocated memory.
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
  *
- * Return: ptr to the newly allocated space, NULL otherwise.
+ * Return: pointer to the resulting string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_length = 0;
-	unsigned int s2_length = 0;
-	unsigned int max_length;
-	char *joined_string;
+	unsigned int len_s1 = 0;
+	unsigned int len_s2 = 0;
+	unsigned int bytes_to_concatenate;
+	char *result;
 	unsigned int i;
 
-	while (s1[s1_length] != '\0')
-		s1_length++;
-
-	while (s2[s2_length] != '\0')
-		s2_length++;
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
+
+	while (s1[len_s1] != '\0')
+	{
+		len_s1++;
+	}
+	while (s2[len_s2] != '\0')
+	{
+		len_s2++;
 	}
 
-	max_length = (n >= s2_length) ? s2_length : n;
-	joined_string = malloc(s1_length + max_length + 1);
+	bytes_to_concatenate = (n < len_s2) ? n : len_s2;
+	result = malloc(len_s1 + bytes_to_concatenate + 1);
 
-	if (joined_string == NULL)
+	if (result == NULL)
 	{
 		return (NULL);
 	}
-
-	for (i = 0; i < s2_length; i++)
+	for (i = 0; i < len_s1; i++)
 	{
-		joined_string[i] = s1[i];
+		result[i] = s1[i];
+	}
+	for (i = 0; i < bytes_to_concatenate; i++)
+	{
+		result[len_s1 + i] = s2[i];
 	}
 
-	for (i = 0; i < max_length; i++)
-	{
-		joined_string[s1_length + i] = s2[i];
-	}
-	joined_string[s1_length + max_length] = '\0';
-	return (joined_string);
+	result[len_s1 + bytes_to_concatenate] = '\0';
+
+	return (result);
 }
