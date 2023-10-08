8daae0ca2c0c2056090510b76f2dd00bd5a65ccb
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-08 16:12:38 -0400

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,6 +10,8 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int i;
+	void *new_ptr;
 	char *source;
 	char *destination;
 
@@ -27,6 +29,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 	{
 		return (ptr);
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
+	{
+		return (NULL);
 	}
 
 	source = ptr;
