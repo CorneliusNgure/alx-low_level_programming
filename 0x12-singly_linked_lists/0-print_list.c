aeee9e2cd0e151a1a19be0ae510cfc766a4e1e4f
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-20 12:10:03 -0400

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints members of list_s struct.
+ * @h: pointer to struct list_s
+ *
+ * Return: no. of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count_nodes = 0;
+
+	while (h != NULL)
+	{	
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}	
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+			count_nodes++;
+			h = h->next;
+	}
+	printf("\n");
+
+	return (count_nodes);
+}
