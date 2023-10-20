18559bad5e19c18b58f93aa79c2ee7ef1dfbd53b
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-20 15:02:18 -0400

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,28 +7,23 @@
  * add_node_end - adds a new node at the end of list_t.
  * @head: double  pointer to the head of the list
  * @str: string to be duplicated and added to the new node
- * 
+ *
  * Return: the address of the new element, or NULL if it fails
  */
 
-list_t *add_node_end(list_t **head, const char *str) {
-    
+list_t *add_node_end(list_t **head, const char *str)
+{
 	list_t *new_node, *last_node;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-	
+
 	new_node = malloc(sizeof(list_t));
-	
+
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
 	new_node->str = strdup(str);
-	
 	if (new_node->str == NULL)
 	{
 		free(new_node);
@@ -36,14 +31,12 @@ list_t *add_node_end(list_t **head, const char *str) {
 	}
 
 	new_node->len = 0;
-	
 	while (str[new_node->len] != '\0')
 	{
 		new_node->len++;
 	}
 
 	new_node->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = new_node;
@@ -58,6 +51,5 @@ list_t *add_node_end(list_t **head, const char *str) {
 
 		last_node->next = new_node;
 	}
-
 	return (new_node);
 }
