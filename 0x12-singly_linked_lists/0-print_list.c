f47bc5de2a43f9ec7c784223cbc099ac84f349b0
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-20 13:55:00 -0400

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,21 @@
 
 size_t print_list(const list_t *h)
 {
-	unsigned int  count_elements = 0;
+	size_t nodes;
 
-	while (h != NULL)
+	nodes = 0;
+
+	while (h)
 	{
+
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
+		nodes++;
 		h = h->next;
-		count_elements++;
 	}
-	return (count_elements);
+	return (nodes);
 }
