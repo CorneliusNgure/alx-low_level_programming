1becdeb825528ce7c3f8bf27a39648bf2ca7a015
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-23 11:40:21 -0400

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees every node in the list.
+ * @head: ptr to the listint_t.
+ *
+ * Return: void.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t current_node;
+
+	while(head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current);
+	}
+}
