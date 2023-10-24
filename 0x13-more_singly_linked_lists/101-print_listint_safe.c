659548393f15683b34f2e2fabd6bf447be83f200
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-24 15:24:42 -0400

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,11 +1,12 @@
-#include <stdlib.h>
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_listint_safe - prints a listint_t linked list safely.
- * @head: a pointer to the head of the list.
- * Return: the number of nodes in the list.
+ * print_listint_safe - prints listint_t struct.
+ * @head: ptr to the struct.
+ *
+ * Return: no. of nodes in the list.
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t node_count = 0, i;
@@ -19,20 +20,22 @@ size_t print_listint_safe(const listint_t *head)
 		{
 			if (head == visited_nodes[i])
 				loop_found = 1;
-			break;
-
+				break;
 		}
 		if (loop_found)
-		{
 			printf("-> [%p] %d\n", (void *)head, head->n);
 			break;
-		}
+
 		printf("[%p] %d\n", (void *)head, head->n);
 
 		new_visited_nodes = malloc((node_count + 1) * sizeof(listint_t *));
+
 		if (new_visited_nodes == NULL)
+		{
 			free(visited_nodes);
-		exit(98);
+			exit(98);
+		}
+
 		for (i = 0; i < node_count; i++)
 		{
 			new_visited_nodes[i] = visited_nodes[i];
@@ -43,8 +46,6 @@ size_t print_listint_safe(const listint_t *head)
 		node_count++;
 		head = head->next;
 	}
-	while (head != NULL)
-		printf("(%p), %d\n", (void *)head, head->n), head = head->next;
 	free(visited_nodes);
 	return (node_count);
 }
