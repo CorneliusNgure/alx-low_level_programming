2ce76aa43777e183aa7e056d915c4a06f5bba4d6
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-31 17:32:18 -0400

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -36,8 +36,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	int file_descriptor, length_of_text;
 	ssize_t data_written;
 
-	if (filename == NULL || text_content == NULL)
-		return (-1);
+	if (filename == NULL)
+		return (1);
+
+	if (text_content == NULL)
+		return (1);
 
 	file_descriptor = open(filename, O_WRONLY | O_APPEND | O_CREAT);
 	if (file_descriptor == -1)
