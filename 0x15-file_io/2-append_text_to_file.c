004b86eeb23a208468a1bc5c59185ef9777dba19
CorneliusNgure <kingcornelius07@gmail.com>
2023-10-31 17:37:02 -0400

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -37,7 +37,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t data_written;
 
 	if (filename == NULL)
-		return (1);
+		return (-1);
 
 	if (text_content == NULL)
 		return (1);
